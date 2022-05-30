58207e8d6648ccebeb6a2563ad90e44491278400
harr1424 <harrington59802@protonmail.com>
2022-05-30 15:01:27 -0600

    Initial Commit

---
+++ Tip Quick!/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tip Quick!
+//
+//  Created by user on 5/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
